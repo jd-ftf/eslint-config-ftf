39c798656c3f6a4ebf562b28127b49117115e67f
zhongzhijie <zhongzhijie@jd.com>
2020-08-25 01:34:44 +0800

    feat: 修改一些实际应用中有问题的规则

--- rules/airbnb.js
+++ rules/airbnb.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['airbnb-base'],
   rules: {
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 1,
+    'no-plusplus': 0 // 关闭禁止一元 -- ++
   }
 };
--- rules/vue.js
+++ rules/vue.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: [
     'plugin:vue/recommended'
-  ]
+  ],
+  rules: {
+    'import/no-unresolved': 0, // 关闭 “import 无法识别的依赖”
+    'vue/no-v-html': 0, // 关闭 “禁止v-html”
+    'vue/require-default-prop': 0 // 关闭 “要求props有默认值”
+  }
 };
