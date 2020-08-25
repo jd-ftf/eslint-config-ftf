904610d5c1b5f21955f032872dc753db20528ced
zhongzhijie <zhongzhijie@jd.com>
2020-08-25 09:42:54 +0800

    feat: 修改一些实际应用中有问题的规则 (#1)
    feat: 修改一些实际应用中有问题的规则

--- __tests__/fixtures/rules.vue
+++ __tests__/fixtures/rules.vue
@@ -1,16 +1,30 @@
 <template>
-  <p>测试</p>
+  <div>
+    <p>测试</p>
+    <div v-html="htmlStr"></div>
+  </div>
 </template>
 
 <script>
+const obj = {
+  proper: 1,
+};
+
 export default {
   props: {
     foo: String,
   },
   data() {
+    const { proper } = obj;
+
     const a = void 0;
+    let i = 0;
+    i++;
     return {
       foo: null,
+      i,
+      proper,
+      htmlStr: '<p style="color: #0083ff;">测试v-shtml</p>',
     };
   },
   computed: {
--- rules/babel.js
+++ rules/babel.js
@@ -3,13 +3,13 @@ module.exports = {
     'babel'
   ],
   rules: {
-    'babel/new-cap': 1,
-    'babel/camelcase': 1,
-    'babel/no-invalid-this': 1,
-    'babel/object-curly-spacing': 1,
-    'babel/quotes': 1,
-    'babel/semi': 1,
-    'babel/no-unused-expressions': 1,
-    'babel/valid-typeof': 1
+    'babel/new-cap': 1, // 构造函数大写开头
+    'babel/camelcase': 1, // 使用驼峰法
+    'babel/no-invalid-this': 1, // 禁止类或对象以外使用 this
+    'babel/object-curly-spacing': 0, // doesn't complain about export x from "mod"; or export * as x from "mod"
+    'babel/quotes': [1, 'single'], // 使用单引号
+    'babel/semi': 1, // doesn't fail when using for await (let something of {}). Includes class properties
+    'babel/no-unused-expressions': 1, // doesn't fail when using do expressions or optional chaining (a?.b())
+    'babel/valid-typeof': 1 // doesn't complain when used with BigInt (typeof BigInt(9007199254740991) === 'bigint')
   }
 };
